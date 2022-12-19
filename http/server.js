eaad3b8ff742e096ee4d9ad1a60c6a2a7c21ec2d
Marcelo Gomes <sistemasespecificos@gmail.com>
2022-12-18 23:35:42 -0300

    Server e outros

---
+++ http/server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+
+http.createServer((req, res) => {
+
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file)
+
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if(err) throw err
+
+            res.end(content)
+        }
+    )
+}).listen(5001, () => console.log('Server is running'))
